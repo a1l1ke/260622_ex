187c6dd0eb8a681e738863b277d86e8b0f2f061b
a1l1ke <onetenthplease@gmail.com>
2026-06-22 09:54:36 +0900

    feat: 숫자 문자열 변환 로직 추가
    - 숫자와 영문 숫자 문자열을 함께 처리하는 변환 로직을 추가했다.
    - 기존 구현을 유지하면서 `solution2` 비교용 구현도 함께 보존했다.

--- src/Solution01.java
+++ src/Solution01.java
@@ -2,7 +2,66 @@ import java.util.*;
 
 public class Solution01 {
     public int solution(String s) {
-        int answer = 0;
+        // 숫자를 판정하는 것
+        Set<String> set = new HashSet();
+        set.add("0");
+        set.add("1");
+        set.add("2");
+        set.add("3");
+        set.add("4");
+        set.add("5");
+        set.add("6");
+        set.add("7");
+        set.add("8");
+        set.add("9");
+        // 글자 -> 숫자 변경
+        Map<String, String> map = new HashMap();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+        String answerStr = "";
+        String tmp = "";
+        for (String v : s.split("")) {
+            if (set.contains(v)) {
+                answerStr += v; // 그냥 숫자니까.
+                continue;
+            }
+            tmp += v;
+            if (map.containsKey(tmp)) {
+                answerStr += map.get(tmp);
+                tmp = ""; // 정확한 숫자가 한 번 감지되면 임시를 비워줌
+            }
+        }
+        int answer = Integer.parseInt(answerStr);
+        return answer;
+    }
+
+    public int solution2(String s) {
+//        System.out.println("s = " + s);
+//        System.out.print("");
+//        for (String v : s.split("")) {
+//        for (char c : s.toCharArray()) { // 위에보다 상대적으로 경량이면서 char -> 연산
+//            System.out.println("v = " + v);
+        String[] numbers = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        // 인덱스가 곧 숫자 이고, 그 인덱스에 대응하는 원소는 그 숫자의 문자열 표현.
+//        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < numbers.length; i++) { // numbers를 돌면서 0 - 9까지를 치환.
+//            char c = s.charAt(i);
+//            System.out.println("c = " + c);
+            // 문자열.replace(바꾸자하는 문자열, 바꿀 문자열)
+            s = s.replace(numbers[i], i + "");
+//            System.out.print("");
+         }
+
+//        int answer = 0;
+        int answer = Integer.parseInt(s); // 문자열을 정수로 바꿔주는...
         return answer;
     }
 
